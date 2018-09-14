e8ef7017c35c332bb4823bce2f118a719094fcbc
Dardin <logandalec@gmail.com>
2018-09-14 15:31:24 -0700

    file creation and initial architecture

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function (){
+
+    // initial list of button and giphy searches available
+    var ams = [];
+
+    //draws search buttons to buttons div
+    function btndraw() {
+        
+        for(var i = 0; i < ams.length; i++){
+          var newbtn = $('<button>');
+        }
+        
+    } 
+
+    // TODO: place ajax in button click function
+    // gets giphy JSON from specified url 
+    $.get({url:'' })
+
+    .then(function(result){
+
+
+    });
+
+
+    // get from submit and call add btn function
+
+
+
+});
